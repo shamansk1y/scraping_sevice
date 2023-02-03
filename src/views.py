d8c9153dd5af487d8d5f0d216fa1846ba76401f4
Shamansk1y <shamansky1225@gmail.com>
2023-02-03 22:24:48 +0100

    com 1.0

---
+++ src/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+import datetime
+
+def home(request):
+    date = datetime.datetime.now().date()
+    name = 'User'
+    con = {'date': date, 'name': name}
+    return render(request, 'home.html', con)
