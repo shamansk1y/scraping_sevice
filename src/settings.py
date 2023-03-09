a9abd9c6eab4971e9fb03a9dc4aaf195bac82e04
Shamansk1y <shamansky1225@gmail.com>
2023-03-09 20:00:46 +0100

    add new foo

--- src/settings.py
+++ src/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'scraping'
+    'scraping',
+    'accounts'
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.MyUser'
