809a56e135adefb5c369e0881b6d104684b731a4
Shamansk1y <shamansky1225@gmail.com>
2023-04-23 20:31:47 +0200

    done

--- scraping/models.py
+++ scraping/models.py
@@ -2,9 +2,6 @@ from django.db import models
 from .utils import from_cyrillic_to_eng
 
 
-def default_urls():
-    return {'work': '', 'dou': '', 'djinni': ''}
-
 class City(models.Model):
     name = models.CharField(max_length=50, verbose_name='Название населенного пункта', unique=True)
     slug = models.CharField(max_length=50, blank=True, unique=True)
@@ -71,7 +68,7 @@ class Error(models.Model):
 class Url(models.Model):
     city = models.ForeignKey('City', on_delete=models.CASCADE, verbose_name='Город')
     language = models.ForeignKey('Language', on_delete=models.CASCADE, verbose_name='Язык программирования')
-    url_data = models.JSONField(default=default_urls())
+    url_data = models.JSONField(default=lambda: {'work': '', 'dou': '', 'djinni': ''})
     
     class Meta:
         unique_together = ('city', 'language')
