feb03f07510434238a24c919f96c55ba6917e0db
Shamansk1y <shamansky1225@gmail.com>
2023-04-24 15:09:53 +0200

    done

--- src/settings/production.py
+++ src/settings/production.py
@@ -91,12 +91,12 @@ WSGI_APPLICATION = 'src.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': DB_NAME,
         'USER': DB_USER,
         'PASSWORD': DB_PASSWORD,
         'HOST': DB_HOST,
-        'PORT': '5432',
+        'PORT': '3306',
     }
 }
 
